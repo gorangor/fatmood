7be055f63f8cbef2dda3f212aabd6067843ec922
Alexander Sahinidis <alex@Alexanders-MacBook-Pro.local>
2018-11-10 15:32:05 -0500

    dragstuff.py

--- dragstuff.py
+++ dragstuff.py
@@ -2,9 +2,30 @@
 
 import pygame
 
-def IngredientclickedOn(event, data):
-    if 300 < pygame.mouse.get_pos()[0] < 467 and 263 < pygame.mouse.get_pos()[1] < 414:
+def IngredientclickedOn(event, data): # update the third to sixth ingredients w/
+                                      # correct file name once completed
+    if 300 < pygame.mouse.get_pos()[0] < 467 and \
+    263 < pygame.mouse.get_pos()[1] < 414:
         return Ingredient("patty.png")
-    elif 487 < pygame.mouse.get_pos()[0] < 603 and 270 < pygame.mouse.get_pos()[1] < 411:
+
+    elif 487 < pygame.mouse.get_pos()[0] < 603 and \
+    270 < pygame.mouse.get_pos()[1] < 411:
         return Ingredient("mushroom.png")
+
+    elif 623 < pygame.mouse.get_pos()[0] < 748 and \
+    270 < pygame.mouse.get_pos()[1] < 411:
+        return Ingredient("mushroom.png")
+
+    elif 778 < pygame.mouse.get_pos()[0] < 912 and \
+    270 < pygame.mouse.get_pos()[1] < 411:
+        return Ingredient("mushroom.png")
+
+    elif 942 < pygame.mouse.get_pos()[0] < 1081 and \
+    270 < pygame.mouse.get_pos()[1] < 411:
+        return Ingredient("mushroom.png")
+
+    elif 1108 < pygame.mouse.get_pos()[0] < 1235 and \
+    270 < pygame.mouse.get_pos()[1] < 411:
+        return Ingredient("mushroom.png")
+
     return None
