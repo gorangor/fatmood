f2eb1b9edcedbd0bdd238c528c65cee0e840ba0c
Alexander Sahinidis <alex@Alexanders-MacBook-Pro.local>
2018-11-10 15:21:02 -0500

    dragstuff.py

--- dragstuff.py
+++ dragstuff.py
@@ -1,10 +1,10 @@
 # Goal is to make dragging ingredient, turning it upside down, and putting it on conveyer belt. Generate an image get filenmae for image, return an Iengredient(image)
 
-
+import pygame
 
 def IngredientclickedOn(event, data):
-    if 300 < event.x < 467 and 263 < event.y < 414:
+    if 300 < pygame.mouse.get_pos()[0] < 467 and 263 < pygame.mouse.get_pos()[1] < 414:
         return Ingredient("patty.png")
-    elif 487 < event.x < 603 and 270 < event.y < 411:
+    elif 487 < pygame.mouse.get_pos()[0] < 603 and 270 < pygame.mouse.get_pos()[1] < 411:
         return Ingredient("mushroom.png")
     return None
